da33a0cae4312e26d14b8574328a66ad2483fed9
ecc97 <carmonaedwin1123@gmail.com>
2024-09-11 11:23:38 -0400

    refactor: adjusting endpoint fetch login, basic

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,5 +1,12 @@
 const apiUrl = process.env.NEXT_PUBLIC_API_URL
 
+interface UserData{
+    username: string
+    email: string
+    password: string
+    role: string
+}
+
 interface UserLogin {
     username: string
     password: string
@@ -23,5 +30,27 @@ export const login = async (userLogin: UserLogin): Promise<UserLogin | null> => 
     } catch (error) {
         console.error('Error in login function:', error)
         throw new Error('Error fetching user data')
+    }
+}
+
+export const register = async (user: UserLogin): Promise<UserData> => {
+    try {
+        const response = await fetch(`${apiUrl}/users`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
+        })
+
+        if (!response.ok) {
+            throw new Error("Server Error")
+        }
+
+        const data: UserData = await response.json()
+        return data
+    } catch (error) {
+        console.error("Error:", error)
+        throw new Error("Error al registrar usuario")
     }
 }
